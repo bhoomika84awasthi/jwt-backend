d59fbce64bfbe1105305f550ae1236d490e53a3c
bhoomika84awasthi <bhoomika84awasthi@gmail.com>
2025-08-29 18:25:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,22 +6,23 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// connect to DB
+// Connect DB
 connectDB();
 
-
+// ✅ Allow only your Vercel frontend
 app.use(cors({
-  origin: "*",  // later you can restrict to your Vercel frontend URL
+  origin: "https://jwt-frontend-a52l.vercel.app",
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization"
 }));
 
 app.use(express.json());
 
-// routes
+// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/protected', require('./routes/protected'));
 
 app.get('/', (req, res) => res.send('JWT Auth API is running ✅'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
